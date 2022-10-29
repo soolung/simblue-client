2a3db21efc621f7e0f3c3860f5b0db9d67ebc9fe
gimhanul <gimhanuly@gmail.com>
2022-10-29 23:27:06 +0900

    ADD :: input componenet label 추가
    - check, radio button label 추가

--- src/components/common/Check/Check.js
+++ src/components/common/Check/Check.js
@@ -3,14 +3,17 @@ import './Check.scss';
 export default function Check(props) {
 
     return (
-        <input
-            className={`check ${props.className}`}
-            type="checkbox"
-            checked={props.isChecked}
-            onChange={props.onChange}
-            value={props.value}
-            name={props.name}
-            id={props.id}
-        />
+        <div className="check-wrapper">
+            <input
+                className={`check ${props.className}`}
+                type="checkbox"
+                checked={props.isChecked}
+                onChange={props.onChange}
+                value={props.value}
+                name={props.name}
+                id={props.id}
+            />
+            <span>{props.label}</span>
+        </div>
     )
 }
