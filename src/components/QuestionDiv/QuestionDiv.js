78803f975723ed0197143438d35f57c220cb36d2
sookyoungwoo <sookyoung2005@gmail.com>
2022-11-24 12:16:40 +0900

    ADD :: question component 더하기 버튼 누르면 추가 (#15)

--- src/components/QuestionDiv/QuestionDiv.js
+++ src/components/QuestionDiv/QuestionDiv.js
@@ -1,28 +1,37 @@
 import './QuestionDiv.scss';
-function QuestionDiv() {
+const QuestionDiv = (props) => {
+
     return (
-        <div className='QuestionDiv-tit'>
-            <div className='QuestionDiv-question'>
-                <div className='QuestionDiv-top'>
-                    <a className='question'><input type='text' placeholder='질문' /></a>
-                    <div className='select'>
-                        <select className='question-type'>
-                            <option value='1'>주관식 질문</option>
-                            <option value='2'>객관식 질문</option>
-                            <option value='3'>체크 박스</option>
-                        </select>
+        <div className='question-div-one-pad'>
+            {props.countList && props.countList.map((item, i) => (
+                <div className='al'>
+                <div className='QuestionDiv-tit' key={i}>
+                    <div className='QuestionDiv-question'>
+                        <div className='QuestionDiv-top'>
+                            <a className='question'><input type='text' placeholder='질문' /></a>
+                            <a className='select'>
+                                <select className='question-type'>
+                                    <option value='1'>주관식 질문</option>
+                                    <option value='2'>객관식 질문</option>
+                                    <option value='3'>체크 박스</option>
+                                </select>
+                            </a>
                         </div>
+                        <div className='QuestionDiv-answer'>
+                            <a className='question'><input type='text' placeholder='주관식 답안' /></a>
+                        </div>
+                        <div className='delete-div'>
+                            <button className='delete-button'>
+                                <img src='/images/delete.svg' />
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div className='QuestionDiv-answer'>
-                    <a className='question'><input type='text' placeholder='주관식 답안' /></a>
                 </div>
-                <div className='delete-div'>
-                    <button className='delete-button'>
-                        <img src='/images/delete.svg' />
-                    </button>
-                </div>
-            </div>
+            ))}
         </div>
     )
 }
+
+
 export default QuestionDiv;
--- src/pages/Create/Creat.js
+++ src/pages/Create/Creat.js
@@ -1,69 +1,79 @@
 import React, { useState } from 'react';
-import InputEmoji from 'react-input-emoji';
 import './Create.scss';
 import QuestionDiv from '../../components/QuestionDiv/QuestionDiv';
-function Create() {
-    const [text, setText] = useState('');
 
-    function handleOnEnter(text) {
-        console.log('enter', text);
-    }
 
-    return (
+    const Create = () => {
+        const [countList, setCountList] = useState([0])
 
-        <section className='Create-section'>
-            <div className='create-tit'>
-                <div className='create-top'>
-                    <div className='create-top-title-img'>
-                        <button>
-                            <InputEmoji
-                                cleanOnEnter
-                                onEnter={handleOnEnter}
-                                maxlength={1}
-                            />
-                        </button>
-                        <a className='create-top-title-input'><input className='create-title-input' type={text} placeholder='제목 입력' /></a>
-                    </div>
-                    <div className='create-top-title-time'>
-                        <div className='create-time-end-time-gigan'>
-                            <div className='create-time-start-time'>
-                                <span className='time-span'>기간</span>
-                                <span>
-                                    <a><input type={text} className='year' placeholder='년(4자)' /></a>
-                                    <a>
-                                        <select className='month'>
-                                            <option value="0">월</option>
-                                        </select>
-                                    </a>
-                                    <a><input type={text} className='day' placeholder='일' /></a>
-                                </span>
-                            </div>
-                            <div className='create-time-end-time'>
-                                <span className='time-span'>~</span>
-                                <span>
-                                    <a><input type={text} className='year' placeholder='년(4자)' /></a>
-                                    <a>
-                                        <select className='month'>
-                                            <option value="0">월</option>
-                                        </select>
-                                    </a>
-                                    <a><input type={text} className='day' placeholder='일' /></a>
-                                </span>
-                            </div>
-                            <div className='sangsi-button'>
-                                <input type="checkbox" /><span>상시</span>
+        const onAddDetailDiv = () => {
+            let countArr = [...countList]
+            let counter = countArr.slice(-1)[0]
+            counter += 1
+            countArr.push(counter)	// index 사용 X
+            // countArr[counter] = counter	// index 사용 시 윗줄 대신 사용	
+            setCountList(countArr)
+        }
+
+        return (
+
+            <section className='Create-section'>
+                <div className='create-tit'>
+                    <div className='create-top'>
+                        <div className='create-top-title-img'>
+                            <button>
+                                이모지
+                            </button>
+                            <a className='create-top-title-input'><input className='create-title-input' type="text" placeholder='제목 입력' /></a>
+                        </div>
+                        <div className='create-top-title-time'>
+                            <div className='create-time-end-time-gigan'>
+                                <div className='create-time-start-time'>
+                                    <span className='time-span'>기간</span>
+                                    <span>
+                                        <a><input type="text" className='year' placeholder='년(4자)' /></a>
+                                        <a>
+                                            <select className='month'>
+                                                <option value="0">월</option>
+                                            </select>
+                                        </a>
+                                        <a><input type="text"  className='day' placeholder='일' /></a>
+                                    </span>
+                                </div>
+                                <div className='create-time-end-time'>
+                                    <span className='time-span'>~</span>
+                                    <span>
+                                        <a><input type="text" className='year' placeholder='년(4자)' /></a>
+                                        <a>
+                                            <select className='month'>
+                                                <option value="0">월</option>
+                                            </select>
+                                        </a>
+                                        <a><input type="text" className='day' placeholder='일' /></a>
+                                    </span>
+                                </div>
+                                <div className='sangsi-button'>
+                                    <input type="checkbox" /><span>상시</span>
+                                </div>
                             </div>
                         </div>
+                        <div className='small-title-tit'>
+                            <a className='small-title'><input type="text" placeholder='소제목 입력' /></a>
+                        </div>
                     </div>
-                    <div className='small-title-tit'>
-                        <a className='small-title'><input type={text} placeholder='소제목 입력' /></a>
+                    <div className='create-question-section' >
+                        <div className='create-question-section-div-plus'>
+                            <div><QuestionDiv  countList={countList}/></div>
+                        </div>
+                    </div>
+                    <div className='question-plus'>
+                        <button className='question-plus-button' onClick={onAddDetailDiv}>
+                            <span>+ 질문 추가</span>
+                        </button>
                     </div>
                 </div>
-                <div className='create-question-section'>
-                    <QuestionDiv/>
-                </div>
-            </div>
-        </section>
-    )
-}
+            </section>
+        )
+    }
+
 export default Create;
