4c86636620a351e27ab950ea4844baec15105276
sookyoungwoo <sookyoung2005@gmail.com>
2022-12-01 13:25:39 +0900

    ADD :: 답안마다 서로 다른 답변 (#18)

--- src/components/QuestionDiv/QuestionDiv.js
+++ src/components/QuestionDiv/QuestionDiv.js
@@ -1,32 +1,84 @@
+import React, { useState } from 'react';
 import './QuestionDiv.scss';
 const QuestionDiv = (props) => {
+
+    const [countList, setCountList] = useState([0])
+
+    const onDeleteDetailDiv = () => {
+        let countArr = [...countList]
+        let counter = countArr.slice(-1)[0]
+        counter -= 1
+        countArr.push(counter)	// index 사용 X
+        // countArr[counter] = counter	// index 사용 시 윗줄 대신 사용	
+        setCountList(countArr)
+    }
+
+    const [selected, setSelected] = useState("");
+
+    const handleSelect = (e) => {
+        setSelected(e.target.value);
+    };
+
 
     return (
         <div className='question-div-one-pad'>
             {props.countList && props.countList.map((item, i) => (
                 <div className='al'>
-                <div className='QuestionDiv-tit' key={i}>
-                    <div className='QuestionDiv-question'>
-                        <div className='QuestionDiv-top'>
-                            <a className='question'><input type='text' placeholder='질문' /></a>
-                            <a className='select'>
-                                <select className='question-type'>
-                                    <option value='1'>주관식 질문</option>
-                                    <option value='2'>객관식 질문</option>
-                                    <option value='3'>체크 박스</option>
-                                </select>
-                            </a>
-                        </div>
-                        <div className='QuestionDiv-answer'>
-                            <a className='question'><input type='text' placeholder='주관식 답안' /></a>
-                        </div>
-                        <div className='delete-div'>
-                            <button className='delete-button'>
-                                <img src='/images/delete.svg' />
-                            </button>
+                    <div className='QuestionDiv-tit' key={i}>
+                        <div className='QuestionDiv-question'>
+                            <div className='QuestionDiv-top'>
+                                <a className='question'><input type='text' placeholder='질문' /></a>
+                                <a className='select'>
+                                    <select className='question-type' onChange={handleSelect} value={selected}>
+                                        <option value='TEXT'>주관식 (단답)</option>
+                                        <option value='TEXTAREA'>주관식 (장문)</option>
+                                        <option value='LINK'>링크</option>
+                                        <option value='RADIO'>객관식 질문</option>
+                                        <option value='CHECKBOX'>체크 박스</option>
+                                    </select>
+                                </a>
+                            </div>
+                            {/* 답안 */}
+                            <div className='QuestionDiv-answer'>
+                                {
+                                    selected === "TEXT" ?
+                                        <div className='question-text'>
+                                            <input type='text' placeholder='주관식 답안' />
+                                        </div>
+                                        : (
+                                            selected === "TEXTAREA" ?
+                                                <div className='question-textarea'>
+                                                    <span><textarea placeholder='주관식 답안' /></span>
+                                                </div>
+                                                :
+                                                (
+                                                    selected === "LINK" ?
+                                                        <div className='question-link'>
+                                                            <input type='text' placeholder='링크 입력' />
+                                                        </div>
+                                                        : (
+                                                            selected === "RADIO" ?
+                                                                <div className='question-radio'>
+                                                                    <span><input type='radio' className='radio' /> <input type='text' placeholder='객관식 질문 입력' className='answertext' /></span>
+                                                                    <span><button>질문 추가</button></span>
+                                                                </div>
+                                                                :
+                                                                <div className='question-check'>
+                                                                    <span><input type='checkbox' className='check' /> <input type='text' placeholder='체크박스 질문 입력' /></span>
+                                                                    <span><button>질문 추가</button></span>
+                                                                </div>
+                                                        )
+                                                )
+                                        )
+                                }
+                            </div>
+                            <div className='delete-div'>
+                                <button className='delete-button' onClick={onDeleteDetailDiv}>
+                                    <img src='/images/delete.svg' />
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
                 </div>
             ))}
         </div>
