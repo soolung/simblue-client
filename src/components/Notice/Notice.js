8762c8ff43de95ee2ccf4c9b3f97829c893e5637
gimhanul <gimhanuly@gmail.com>
2022-12-13 08:12:20 +0900

    CHORE :: datetime 한글로 format

--- src/components/Notice/Notice.js
+++ src/components/Notice/Notice.js
@@ -1,11 +1,12 @@
 import "./Notice.scss"
+import {dateKoreanFormatter} from "../../utils/etc/DateTimeFormatter";
 
 export default function Notice({text, author, time, isPinned}) {
     return (
         <>
             <div className="notice">
                 <p className="notice-text">{text}</p>
-                <span className="notice-info">{author} 선생님 ∙ {time}</span>
+                <span className="notice-info">{author} 선생님 ∙ {dateKoreanFormatter(time)}</span>
                 { isPinned ? <img src="/images/pin.svg" alt="pin" className="pin"/> : <></>}
             </div>
         </>
