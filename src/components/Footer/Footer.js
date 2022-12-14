1abeff6aabca21b41b92df42ad81399c148537ff
gimhanul <gimhanuly@gmail.com>
2022-12-14 13:04:57 +0900

    CHORE :: create 할 때 푸터 없앰

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,10 @@
 import "./Footer.scss";
+import {useLocation} from "react-router-dom";
 
 export default function Footer() {
+    const { pathname } = useLocation();
+    if (pathname === "/create") return null;
+
     return (
         <footer>
             <p>우수경과 김한울애 수행평가 그루브, 수렁.</p>
