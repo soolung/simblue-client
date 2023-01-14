de690f41bb1ef25f235825c253a96e33b8865f78
kimhyerin0909 <khyerin0909@gmail.com>
2023-01-14 22:36:00 +0900

    ADD :: login page
    모달 창에서 페이지로 바꿈

---
+++ src/pages/Auth/Login.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Login.scss";
+import TextBox from "../../components/common/TextBox/TextBox";
+
+export const Login = () => {
+  return (
+    <div className="login">
+      <div className="img-box">
+        <img alt="simblue" src="https://ifh.cc/g/H0wG7w.png" />
+        <p className="login-insa">환영합니다!</p>
+      </div>
+      <div className="login-form">
+        <div className="login-title">
+          <span>로그인</span>
+          <img alt="welcome" src="https://ifh.cc/g/VBj8B5.png" />
+        </div>
+        <p className="login-subtitle">학교 계정으로 로그인</p>
+        <div className="input-box">
+          <TextBox placeholder="이메일을 입력해주세요."></TextBox>
+          <TextBox type="password" placeholder="비밀번호을 입력해주세요."></TextBox>
+        </div>
+        <div className="input-box">
+          <button className="login-btn">로그인</button>
+          <button className="login-google-btn ">
+            <img src="https://ifh.cc/g/nNDjB0.png" alt="google" />
+            <span>구글 계정으로 로그인</span>
+          </button>
+        </div>
+        <div className="to-signup">
+          아직 회원이 아니신가요?
+          <span> 구글 계정으로 회원가입</span>
+        </div>
+      </div>
+    </div>
+  );
+};
