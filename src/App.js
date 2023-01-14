97dcddf6946bbd5c4d32fd59384b3d69f843b4bf
kimhyerin0909 <khyerin0909@gmail.com>
2023-01-14 22:35:16 +0900

    ADD :: login route

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
-import ReactModal from 'react-modal';
-import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom';
-import Header from './components/Header/Header';
-import Main from './pages/Main/Main';
-import Look from './pages/Look/Look';
+import ReactModal from "react-modal";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Main from "./pages/Main/Main";
+import Look from "./pages/Look/Look";
 import Record from "./pages/Record/Record";
 import Create from "./pages/Create/Create";
 import ApplicationManagement from "./pages/Application/ApplicationManagement";
 import Footer from "./components/Footer/Footer";
-import {useEffect} from "react";
+import { useEffect } from "react";
+import { Login } from "./pages/Auth/Login";
 
-const WithLogin = ({authority=null, children}) => {
-    const navigate = useNavigate();
-    const actualAuthority = localStorage.getItem("authority");
-    useEffect(() => {
-        if (!actualAuthority || (authority != null && authority !== actualAuthority)) {
-            navigate("/");
-        }
-    }, []);
+const WithLogin = ({ authority = null, children }) => {
+  const navigate = useNavigate();
+  const actualAuthority = localStorage.getItem("authority");
+  useEffect(() => {
+    if (!actualAuthority || (authority != null && authority !== actualAuthority)) {
+      navigate("/");
+    }
+  }, []);
 
-    return <>{children}</>;
+  return <>{children}</>;
 };
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                <Header/>
-                <Routes>
-                    <Route path='/' element={<Main/>}/>
-                    <Route path='/look' element={<Look/>}/>
-                    <Route path='/record' element={<WithLogin children={<Record/>}/>}/>
-                    <Route path='/create' element={<WithLogin authority="ROLE_TEACHER" children={<Create/>}/>}/>
-                    <Route path='/application/:id' element={<WithLogin authority="ROLE_TEACHER" children={<ApplicationManagement/>}/>}/>
-                </Routes>
-                <Footer/>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/look" element={<Look />} />
+          <Route path="/record" element={<WithLogin children={<Record />} />} />
+          <Route path="/create" element={<WithLogin authority="ROLE_TEACHER" children={<Create />} />} />
+          <Route path="/application/:id" element={<WithLogin authority="ROLE_TEACHER" children={<ApplicationManagement />} />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
 
-ReactModal.setAppElement('#root')
+ReactModal.setAppElement("#root");
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,60 +1,56 @@
-import './Header.scss';
-import LoginModal from '../Modal/Login/LoginModal';
-import {useState} from "react";
-import {Link} from "react-router-dom";
-import {useRecoilState} from "recoil";
-import {userState} from "../../utils/atom/user";
+import "./Header.scss";
+import LoginModal from "../Modal/Login/LoginModal";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { userState } from "../../utils/atom/user";
 import useModal from "../../hooks/useModal";
 
 export default function Header() {
-    const {openModal, closeModal} = useModal();
-    const [user, setUser] = useRecoilState(userState);
-    const [searchText, setSearchText] = useState("");
-    const [searchTextOnFocus, setSearchTextOnFocus] = useState(false);
+  const { openModal, closeModal } = useModal();
+  const [user, setUser] = useRecoilState(userState);
+  const [searchText, setSearchText] = useState("");
+  const [searchTextOnFocus, setSearchTextOnFocus] = useState(false);
 
-    const toggleSearchTextOnFocus = e => {
-        setSearchTextOnFocus(!searchTextOnFocus)
-    }
+  const toggleSearchTextOnFocus = (e) => {
+    setSearchTextOnFocus(!searchTextOnFocus);
+  };
 
-    const writeSearchText = e => {
-        setSearchText(e.target.value)
-    }
+  const writeSearchText = (e) => {
+    setSearchText(e.target.value);
+  };
 
-    const resetSearchText = e => {
-        setSearchText("");
-    }
+  const resetSearchText = (e) => {
+    setSearchText("");
+  };
 
-    const logout = () => {
-        localStorage.clear();
-        setUser({
-            token: null,
-            authority: null,
-            name: null,
-        })
-    }
+  const logout = () => {
+    localStorage.clear();
+    setUser({
+      token: null,
+      authority: null,
+      name: null,
+    });
+  };
 
-    const openLoginModal = () => {
-        openModal(
-            <LoginModal
-                closeModal={closeModal}
-            />
-        )
-    }
+  const openLoginModal = () => {
+    openModal(<LoginModal closeModal={closeModal} />);
+  };
 
-    return (
-        <>
-            <header>
-                <div className='header-inner'>
-                    <Link to="/">
-                        <div className='header_logo'>
-                            <img src='https://ifh.cc/g/cs0mAl.png' alt='logo'/>
-                            {/* {checkObjectIsEmpty(user) ? */}
-                            {/* <> */}
-                            <div className='logo-font'>
-                                <p className='logo-simblue'>심청이</p>
-                                <p className='logo-bssm'>부산소프트웨어마이스터고등학교</p>
-                            </div>
-                            {/* </>
+  return (
+    <>
+      <header>
+        <div className="header-inner">
+          <Link to="/">
+            <div className="header_logo">
+              <img src="https://ifh.cc/g/cs0mAl.png" alt="logo" />
+              {/* {checkObjectIsEmpty(user) ? */}
+              {/* <> */}
+              <div className="logo-font">
+                <p className="logo-simblue">심청이</p>
+                <p className="logo-bssm">부산소프트웨어마이스터고등학교</p>
+              </div>
+              {/* </>
                             :
                             <>
                                 <dd className='logo-simchung'>
@@ -65,25 +61,35 @@ export default function Header() {
                                 </dt>
                             </>
                         } */}
-                        </div>
-                    </Link>
-                    <div className='header_list'>
-                        <div className='header_list--list'>
-                            {/* {checkObjectIsEmpty(user) ?
+            </div>
+          </Link>
+          <div className="header_list">
+            <div className="header_list--list">
+              {/* {checkObjectIsEmpty(user) ?
                             <> */}
-                            <ul className='header-nav'>
-                                <li className='header-nav-li'><Link to="/look">둘러보기</Link></li>
-                                {user?.authority &&
-                                    <li className='header-nav-li'><Link to="/record">기록보기</Link></li>
-                                }
-                                {user?.authority === "ROLE_TEACHER" ?
-                                    <li className='header-nav-li'><Link to="/create">만들기</Link></li>
-                                    :
-                                    <></>
-                                }
-                                <li className='header-nav-li'><a href="https://soolung.notion.site/c351137b354147d6b54b9beabc745caa" target="_blank">도움말</a></li>
-                            </ul>
-                            {/* </>
+              <ul className="header-nav">
+                <li className="header-nav-li">
+                  <Link to="/look">둘러보기</Link>
+                </li>
+                {user?.authority && (
+                  <li className="header-nav-li">
+                    <Link to="/record">기록보기</Link>
+                  </li>
+                )}
+                {user?.authority === "ROLE_TEACHER" ? (
+                  <li className="header-nav-li">
+                    <Link to="/create">만들기</Link>
+                  </li>
+                ) : (
+                  <></>
+                )}
+                <li className="header-nav-li">
+                  <a href="https://soolung.notion.site/c351137b354147d6b54b9beabc745caa" target="_blank">
+                    도움말
+                  </a>
+                </li>
+              </ul>
+              {/* </>
                             :
                             <>
                                 <ul>
@@ -93,27 +99,37 @@ export default function Header() {
                                 </ul>
                             </>
                         } */}
-                        </div>
-                    </div>
+            </div>
+          </div>
 
-                    <div className="search">
-                        <input type="text" placeholder="검색어를 입력해주세요." value={searchText} onChange={writeSearchText}
-                               onFocus={toggleSearchTextOnFocus} onBlur={toggleSearchTextOnFocus}/>
-                        <button
-                            className={"search-delete " + (searchText.length > 0 && searchTextOnFocus ? "search-delete-show" : "search-delete-no")}
-                            onClick={resetSearchText}/>
-                        <input type="image" className="search-go" src={"https://ifh.cc/g/nXpwoz.png"} alt="search-go"/>
-                    </div>
-                    <div className='header_login_button'>
-                        {
-                            user?.authority ?
-                                <button onClick={logout} className='login-button'>{user.name}</button>
-                                :
-                                <button onClick={openLoginModal} className='login-button'>로그인</button>
-                        }
-                    </div>
-                </div>
-            </header>
-        </>
-    )
+          <div className="search">
+            <input
+              type="text"
+              placeholder="검색어를 입력해주세요."
+              value={searchText}
+              onChange={writeSearchText}
+              onFocus={toggleSearchTextOnFocus}
+              onBlur={toggleSearchTextOnFocus}
+            />
+            <button
+              className={"search-delete " + (searchText.length > 0 && searchTextOnFocus ? "search-delete-show" : "search-delete-no")}
+              onClick={resetSearchText}
+            />
+            <input type="image" className="search-go" src={"https://ifh.cc/g/nXpwoz.png"} alt="search-go" />
+          </div>
+          <div className="header_login_button">
+            {user?.authority ? (
+              <button onClick={logout} className="login-button">
+                {user.name}
+              </button>
+            ) : (
+              <li className="login-button">
+                <Link to="/login">로그인</Link>
+              </li>
+            )}
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
