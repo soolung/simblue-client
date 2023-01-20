676970a3fabfc0fab426c3444567167931225fd6
sookyoungwoo <sookyoung2005@gmail.com>
2023-01-20 23:52:49 +0900

    ADD :: header 수정

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,112 +1,139 @@
-import './Header.scss';
-import LoginModal from '../Modal/Login/LoginModal';
-import {useState} from "react";
-import {Link} from "react-router-dom";
-import {useRecoilState} from "recoil";
-import {userState} from "../../utils/atom/user";
+import "./Header.scss";
+import LoginModal from "../Modal/Login/LoginModal";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { userState } from "../../utils/atom/user";
 import useModal from "../../hooks/useModal";
-import styled, {css} from "styled-components";
-import {AiOutlineBars} from 'react-icons/ai';
-import useMedia from '../../hooks/useMedia';
+import styled, { css } from "styled-components";
+import { AiOutlineBars } from "react-icons/ai";
+import useMedia from "../../hooks/useMedia";
 export default function Header() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
 
-    const [isNavExpanded, setIsNavExpanded] = useState(false)
+  const { openModal, closeModal } = useModal();
+  const [user, setUser] = useRecoilState(userState);
+  const [searchText, setSearchText] = useState("");
+  const [searchTextOnFocus, setSearchTextOnFocus] = useState(false);
 
-    const {openModal, closeModal} = useModal();
-    const [user, setUser] = useRecoilState(userState);
-    const [searchText, setSearchText] = useState("");
-    const [searchTextOnFocus, setSearchTextOnFocus] = useState(false);
+  const toggleSearchTextOnFocus = (e) => {
+    setSearchTextOnFocus(!searchTextOnFocus);
+  };
 
-    const toggleSearchTextOnFocus = e => {
-        setSearchTextOnFocus(!searchTextOnFocus)
-    }
+  const writeSearchText = (e) => {
+    setSearchText(e.target.value);
+  };
 
-    const writeSearchText = e => {
-        setSearchText(e.target.value)
-    }
+  const resetSearchText = (e) => {
+    setSearchText("");
+  };
 
-    const resetSearchText = e => {
-        setSearchText("");
-    }
+  const logout = () => {
+    localStorage.clear();
+    setUser({
+      token: null,
+      authority: null,
+      name: null,
+    });
+  };
 
-    const logout = () => {
-        localStorage.clear();
-        setUser({
-            token: null,
-            authority: null,
-            name: null,
-        })
-    }
+  const openLoginModal = () => {
+    openModal(<LoginModal closeModal={closeModal} />);
+  };
 
-    const openLoginModal = () => {
-        openModal(
-            <LoginModal
-                closeModal={closeModal}
-            />
-        )
-    }
-
-    return (
-        <>
-            <header>
-                <div className='header-inner'>
-                    <div className='header-logo'>
-                        <Link to="/">
-                            <ul className='header_logo'>
-                                <li><img src='https://ifh.cc/g/cs0mAl.png' alt='logo'/></li>
-                                {/*a 태그로 하고 싶은데 외안대*/}
-                                <li className='logo-font'>
-                                    <p className='logo-simblue'>심청이</p>
-                                    <p className='logo-bssm'>부산소프트웨어마이스터고등학교</p>
-                                </li>
-                            </ul>
-                        </Link>
-                    </div>
-
+  return (
+    <>
+      <header>
+        <div className="header-header">
+          <div className="header-inner">
+            <div className="header-logo">
+              <Link to="/">
+                <ul className="header_logo">
+                  <li>
+                    <img src="https://ifh.cc/g/cs0mAl.png" alt="logo" />
+                  </li>
+                  {/*a 태그로 하고 싶은데 외안대*/}
+                  <li className="logo-font">
+                    <p className="logo-simblue">심청이</p>
+                    <p className="logo-bssm">부산소프트웨어마이스터고등학교</p>
+                  </li>
+                </ul>
+              </Link>
+            </div>
+            {/* 
                     <button
                         className="hamburger"
                         onClick={() => {
                             setIsNavExpanded(!isNavExpanded)
                         }}
                     >
-                    <div className={
-                        isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-                    }>
-                        <ul className='header-category-ul'>
-                            <li className='header-nav-li'><Link to="/look"><a>둘러보기</a></Link></li>
-                            {user?.authority &&
-                                <li className='header-nav-li'><Link to="/record"><a>기록보기</a></Link></li>
-                            }
-                            {user?.authority === "ROLE_TEACHER" ?
-                                <li className='header-nav-li'><Link to="/create"><a>만들기</a></Link></li>
-                                :
-                                <></>
-                            }
-                        </ul>
+                        </button> */}
 
+            <div className="header-category-total">
+              <ul className="header-category-ul">
+                <li className="header-nav-li">
+                  <Link to="/look">
+                    <a>둘러보기</a>
+                  </Link>
+                </li>
+                {user?.authority && (
+                  <li className="header-nav-li">
+                    <Link to="/record">
+                      <a>기록보기</a>
+                    </Link>
+                  </li>
+                )}
+                {user?.authority === "ROLE_TEACHER" ? (
+                  <li className="header-nav-li">
+                    <Link to="/create">
+                      <a>만들기</a>
+                    </Link>
+                  </li>
+                ) : (
+                  <></>
+                )}
+              </ul>
+            </div>
 
-                        <div className="search">
-                            <input type="text" placeholder="검색어를 입력해주세요." value={searchText} onChange={writeSearchText}
-                                   onFocus={toggleSearchTextOnFocus} onBlur={toggleSearchTextOnFocus}/>
-                            <button
-                                className={"search-delete " + (searchText.length > 0 && searchTextOnFocus ? "search-delete-show" : "search-delete-no")}
-                                onClick={resetSearchText}/>
-                            <input type="image" className="search-go" src={"https://ifh.cc/g/nXpwoz.png"}
-                                   alt="search-go"/>
-                        </div>
-                        <div className='header_login_button'>
-                            {
-                                user?.authority ?
-                                    <button onClick={logout} className='login-button'>{user.name}</button>
-                                    :
-                                    <button onClick={openLoginModal} className='login-button'>로그인</button>
-                            }
-                        </div>
-                    </div>
-                </div>
-            </header>
-        </>
-    )
+            <div className="search">
+              <input
+                type="text"
+                placeholder="검색어를 입력해주세요."
+                value={searchText}
+                onChange={writeSearchText}
+                onFocus={toggleSearchTextOnFocus}
+                onBlur={toggleSearchTextOnFocus}
+              />
+              <button
+                className={
+                  "search-delete " +
+                  (searchText.length > 0 && searchTextOnFocus
+                    ? "search-delete-show"
+                    : "search-delete-no")
+                }
+                onClick={resetSearchText}
+              />
+              <input
+                type="image"
+                className="search-go"
+                src={"https://ifh.cc/g/nXpwoz.png"}
+                alt="search-go"
+              />
+            </div>
+            <div className="header_login_button">
+              {user?.authority ? (
+                <button onClick={logout} className="login-button">
+                  {user.name}
+                </button>
+              ) : (
+                <button onClick={openLoginModal} className="login-button">
+                  로그인
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
-
-
