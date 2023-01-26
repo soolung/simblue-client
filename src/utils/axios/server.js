2dfda987d6a45620a501a01e00cb48377567bcb8
sookyoungwoo <sookyoung2005@gmail.com>
2023-01-26 14:40:56 +0900

    TEST

--- src/utils/axios/server.js
+++ src/utils/axios/server.js
@@ -1,36 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
 const server = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
-    timeout: 10000
+  baseURL: "http://15.164.60.153:8080",
+  timeout: 10000,
 });
 
 server.interceptors.request.use(
-    function (config) {
-        return config;
-    },
-    function (error) {
-        return Promise.reject(error);
-    }
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
 );
 
 server.interceptors.response.use(
-    function (response) {
-        return response;
-    },
+  function (response) {
+    return response;
+  },
 
-    function (error) {
-        if (error.response && error.response.status) {
-            console.error(error.response.data)
-            // switch (error.response.status) {
-            //     case 401:
-            //         break;
-            //     default:
-            //         return Promise.reject(error);
-            // }
-        }
-
-        return Promise.reject(error);
+  function (error) {
+    if (error.response && error.response.status) {
+      console.error(error.response.data);
+      // switch (error.response.status) {
+      //     case 401:
+      //         break;
+      //     default:
+      //         return Promise.reject(error);
+      // }
     }
+
+    return Promise.reject(error);
+  }
 );
 export default server;
