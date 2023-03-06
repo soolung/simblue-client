89056955f2008ef6ab897c0bb324e69c01cf69b9
sookyoungwoo <sookyoung2005@gmail.com>
2023-03-07 00:33:57 +0900

    ADD :: 학생 기록보기

--- src/components/Record/StudentApplication/StudentApplication.js
+++ src/components/Record/StudentApplication/StudentApplication.js
@@ -1,21 +1,31 @@
+import "./StudentApplication.scss";
+import {FaTrash} from "react-icons/fa";
 export default function StudentApplication(props) {
   return (
     <>
-      <div>
-        <div>
-          <p>
-            <p>
-              <a>{props.emoji}</a>
-              <a>{props.title}</a>
+      <div className="student-application">
+        <div className="student-application-a">
+          <p className="student-application-a-left">
+            <p className="student-application-left-left">
+              <a className="student-application-emoji">{props.emoji}</a>
+              <a className="sutdent-application-title">{props.title}</a>
             </p>
-            <p>
-                {props.repliedAt} 에 신청함
+            <p className="student-applicaiton-left-repliedAt">
+              {props.repliedAt} 에 신청함
             </p>
           </p>
-          <p></p>
+          <p className="student-application-a-right">
+            {props.status === "IN_PROGRESS" ? (
+              <p className="application-student-ing">진행중</p>
+            ) : (
+              <p className="application-student-done">마감</p>
+            )}
+            <p className="application-student-right-delete">
+              <FaTrash/>
+            </p>
+          </p>
         </div>
       </div>
     </>
   );
 }
-
