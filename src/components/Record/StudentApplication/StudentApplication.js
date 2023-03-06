d141ac75a0b749e3650c73c28b05cf00ba5e0ee9
우수경 <sookyoung@usugyeong-ui-MacBookAir.local>
2023-03-06 20:16:40 +0900

    ADD :: 선생님 기록보기 완료햇읍니다 ㅎㅎ

---
+++ src/components/Record/StudentApplication/StudentApplication.js
@@ -0,0 +1,18 @@
+export default function StudentApplication(props) {
+  return (
+    <>
+      <div>
+        <div>
+          <p>
+            <p>
+              <a>{props.emoji}</a>
+              <a>{props.title}</a>
+            </p>
+            <p></p>
+          </p>
+          <p></p>
+        </div>
+      </div>
+    </>
+  );
+}
