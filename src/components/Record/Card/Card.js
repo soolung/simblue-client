7b4cb47ed458c9e61152fcfda65199a3e9ad0a88
gimhanul <gimhanuly@gmail.com>
2023-03-07 16:01:34 +0900

    FIX :: 수정 페이지로 이동 안 하는 문제

--- src/components/Record/Card/Card.js
+++ src/components/Record/Card/Card.js
@@ -1,6 +1,6 @@
 import "./Card.scss";
 import { HiPencil } from "react-icons/hi";
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Card({ applicationId, emoji, title, status, endDate, numberOfReplies }) {
 
@@ -11,37 +11,32 @@ function Card({ applicationId, emoji, title, status, endDate, numberOfReplies })
   const result = Math.ceil(gap / (1000 * 60 * 60 * 24));
 
   return (
-    <>
-      <div className="record-card"
-           onClick={() => navigate(`/application/${applicationId}/manage`)}
-      >
-        <div className="record-card-com">
-          <div className="record-card-top">
-            <span className="record-card-top-emoji emoji">{emoji}</span>
-            <span className="record-card-top-title">{title}</span>
-          </div>
-          <div className="record-card-bottom">
-            <p className="record-card-bottom-p">
-              {status === "ALWAYS" ? (
-                <span>상시</span>
-              ) : status === "NOT_STARTED" ? (
-                <span>시작 전</span>
-              ) : status === "IN_PROGRESS" ? (
-                <span>{result}일 남음</span>
-              ) : (
-                <span>완료됨</span>
-              )}
-              <span className="record-card-bar">|</span>
-              <span>{numberOfReplies}개의 신청</span>
-            </p>
-            <HiPencil
-              className="record-card-bottom-pen"
-              onClick={() => navigate(`/application/${applicationId}/update`)}
-            />
-          </div>
+    <Link className="record-card" to={`/application/${applicationId}/manage`}>
+      <div className="record-card-com">
+        <div className="record-card-top">
+          <span className="record-card-top-emoji emoji">{emoji}</span>
+          <span className="record-card-top-title">{title}</span>
+        </div>
+        <div className="record-card-bottom">
+          <p className="record-card-bottom-p">
+            {status === "ALWAYS" ? (
+              <span>상시</span>
+            ) : status === "NOT_STARTED" ? (
+              <span>시작 전</span>
+            ) : status === "IN_PROGRESS" ? (
+              <span>{result}일 남음</span>
+            ) : (
+              <span>완료됨</span>
+            )}
+            <span className="record-card-bar">|</span>
+            <span>{numberOfReplies}개의 신청</span>
+          </p>
+          <Link to={`/application/${applicationId}/update`}>
+            <HiPencil className="record-card-bottom-pen"/>
+          </Link>
         </div>
       </div>
-    </>
+    </Link>
   );
 }
 
