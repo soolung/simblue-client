bc970bed964d536b467e56070433484aa599270e
gimhanul <gimhanuly@gmail.com>
2023-03-08 16:58:34 +0900

    ADD :: Modal component
    - modal component로 confirm modal 감쌈
    - modal overlay 안 되는 문제 해결

--- src/components/Modal/ConfirmModal/ConfirmModal.js
+++ src/components/Modal/ConfirmModal/ConfirmModal.js
@@ -1,7 +1,8 @@
+import Modal from "react-modal";
 import useModal from "../../../hooks/useModal";
 import "./ConfirmModal.scss";
 
-const Modal = ({ title, description, onConfirm }) => {
+const ConfirmModal = ({ title, description, onConfirm }) => {
   const { closeModal } = useModal();
 
   const onClick = () => {
@@ -10,30 +11,31 @@ const Modal = ({ title, description, onConfirm }) => {
   };
 
   return (
-    <div className="confirm-modal">
-      <div className="confirm-modal-confirm">
-        <div className="confirm-modal-wrap">
-          <div className="confirm-modal-wrap-textbox">
-            <p className="confirm-text-title">{title}</p>
-            <p className="confirm-text-content">{description}</p>
-          </div>
-          <div className="confirm-modal-wrap-buttonbox">
-            <div className="confirm-modal-wrap-box">
-              <button
-                className="confirm-modal-wrap-cancel"
-                onClick={closeModal}
-              >
-                취소
-              </button>
-              <button className="confirm-modal-wrap-change" onClick={onClick}>
-                확인
-              </button>
-            </div>
+    <Modal isOpen={true} onRequestClose={closeModal}
+           className="modal confirm-modal"
+           overlayClassName="modal-overlay"
+    >
+      <div className="confirm-modal-wrap">
+        <div className="confirm-modal-wrap-textbox">
+          <p className="confirm-text-title">{title}</p>
+          <p className="confirm-text-content">{description}</p>
+        </div>
+        <div className="confirm-modal-wrap-buttonbox">
+          <div className="confirm-modal-wrap-box">
+            <button
+              className="confirm-modal-wrap-cancel"
+              onClick={closeModal}
+            >
+              취소
+            </button>
+            <button className="confirm-modal-wrap-change" onClick={onClick}>
+              확인
+            </button>
           </div>
         </div>
       </div>
-    </div>
+    </Modal>
   );
 };
 
-export default Modal;
+export default ConfirmModal;
