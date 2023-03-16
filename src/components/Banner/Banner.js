9fad2b30b5bd52cdc469d81733159a2f00585da7
sookyoungwoo <sookyoung2005@gmail.com>
2023-03-16 23:11:08 +0900

    ADD :: 배너openModal

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,43 +1,51 @@
-import './Banner.scss'
-import {Swiper, SwiperSlide } from 'swiper/react';
+import "./Banner.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Pagination, Autoplay, EffectFade } from "swiper";
 import "swiper/scss";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
 import "swiper/css/effect-fade";
-import {Link} from "react-router-dom";
-import { useRecoilValue } from 'recoil';
-import { userState } from '../../utils/atom/user';
+import { Link } from "react-router-dom";
+import { FaPen } from "react-icons/fa";
+import useModal from "../../hooks/useModal";
+import BannerMaker from "../Modal/BannerMaker/BannerMaker";
+SwiperCore.use([Pagination, Autoplay, EffectFade]);
 
-SwiperCore.use([Pagination, Autoplay, EffectFade])
+function Banner(props) {
+  const { openModal } = useModal();
 
-
-function Banner(props){
-    return(
-        <div className='banner_tit'>
-            <Swiper
-                className='banner'
-                loop={true}
-                effect={"fade"}
-                spaceBetween={50}
-                slidesPerView={1}
-                pagination={{
-                    type : 'none'
-                }}
-                autoplay={{delay: 5000}}
-                watchOverflow={true}
-            >
-                {props.banner.map((b, index) => (
-                    <SwiperSlide key={index}>
-                        <Link to ="">
-                            <img className='banner--image' src={b.img} alt="banner"/>
-                        </Link>
-                    </SwiperSlide>
-                ))
-                }
-            </Swiper>
-        </div>
-    )
+  return (
+    <div className="banner_tit">
+      <Swiper
+        className="banner"
+        loop={true}
+        effect={"fade"}
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{
+          type: "none",
+        }}
+        autoplay={{ delay: 5000 }}
+        watchOverflow={true}
+      >
+        {props.banner.map((b, index) => (
+          <SwiperSlide key={index}>
+            <Link to="">
+              <img className="banner--image" src={b.img} alt="banner" />
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="banner-maker-button">
+        <FaPen
+          onClick={(e) => {
+            e.preventDefault();
+            openModal(<BannerMaker title="배너 등록" />);
+          }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
---
+++ src/components/Modal/BannerMaker/BannerMaker.js
@@ -0,0 +1,39 @@
+import Modal from "react-modal";
+import useModal from "../../../hooks/useModal";
+import "./BannerMaker.scss";
+
+const BannerMaker = ({ title }) => {
+  const { closeModal } = useModal();
+
+  const onClick = () => {
+    closeModal();
+  };
+
+  return (
+    <Modal
+      isOpen={true}
+      onRequestClose={closeModal}
+      className="modal confirm-modal"
+      overlayClassName="modal-overlay"
+    >
+      <div className="confirm-modal-wrap">
+        <div className="confirm-modal-wrap-textbox">
+          <p className="confirm-text-title">{title}</p>
+          <p className="confirm-text-content">ff</p>
+        </div>
+        <div className="confirm-modal-wrap-buttonbox">
+          <div className="confirm-modal-wrap-box">
+            <button className="confirm-modal-wrap-cancel" onClick={closeModal}>
+              취소
+            </button>
+            <button className="confirm-modal-wrap-change" onClick={onClick}>
+              확인
+            </button>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default BannerMaker;
