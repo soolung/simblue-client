7e004b453a204925ccd22630f2b71413dda9b437
Thinker <102664109+YJH2848@users.noreply.github.com>
2023-03-20 20:03:27 +0900

    ADD :: id찾아서 draggableId에 넣어주면 드래그완성

--- src/components/Record/RecordKanban/RecordKanban.js
+++ src/components/Record/RecordKanban/RecordKanban.js
@@ -1,29 +1,49 @@
 import "./RecordKanban.scss";
-import Card from '../Card/Card';
+import Card from "../Card/Card";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 export default function RecordKanban({ emoji, title, data }) {
   return (
     <>
-      <div className="record-kanban">
-        <div className="record-kanban-top">
-          <span className="emoji">{emoji}</span>
-          <span className="record-kanban-title">{title}</span>
-        </div>
-        <div className="record-kanban-bottom">
-          {data?.map((k, index) => (
-            <Card
-              applicationId={k.applicationId}
-              emoji={k.emoji}
-              title={k.title}
-              endDate={k.endDate}
-              starteDate={k.startDate}
-              status={k.status}
-              numberOfReplies={k.numberOfReplies}
-              key={index}
-            />
-          ))}
-        </div>
-      </div>
+      <DragDropContext>
+        <Droppable droppableId="record-kanban">
+          {provided => (
+            <div
+              className="record-kanban"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              <div className="record-kanban-top">
+                <span className="emoji">{emoji}</span>
+                <span className="record-kanban-title">{title}</span>
+              </div>
+              <div className="record-kanban-bottom">
+                {data?.map((k, index) => (
+                  <Draggable key={index} draggableId={index} index={index}>
+                    {provided => (
+                      <div
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                      >
+                        <Card
+                          applicationId={k.applicationId}
+                          emoji={k.emoji}
+                          title={k.title}
+                          endDate={k.endDate}
+                          starteDate={k.startDate}
+                          status={k.status}
+                          numberOfReplies={k.numberOfReplies}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+              </div>
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </>
   );
 }
