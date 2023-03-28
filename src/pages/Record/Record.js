5d47e8f84cdbe08701355f597e48f77478590dfb
gimhanul <gimhanuly@gmail.com>
2023-03-29 08:39:21 +0900

    ADD :: result table null 처리

--- src/pages/Record/Record.js
+++ src/pages/Record/Record.js
@@ -5,9 +5,10 @@ import { useRecoilValue } from "recoil";
 import { userState } from "../../utils/atom/user";
 import RecordKanban from "../../components/Record/RecordKanban/RecordKanban";
 import ReplyRecord from "../../components/Record/ReplyRecord/ReplyRecord";
+import Loading from '../../components/common/Loading/Loading';
 
 export default function Record() {
-  const { data } = useQuery("getMyApplications", getMyApplications);
+  const { data, isLoading } = useQuery("getMyApplications", getMyApplications);
   const user = useRecoilValue(userState);
 
   return (
@@ -23,51 +24,55 @@ export default function Record() {
             )}
           </p>
         </div>
-        {data?.authority === "ROLE_TEACHER" ? (
-          <div className="record-body">
-            <RecordKanban
-              emoji="📌"
-              title="상시"
-              data={data?.applicationMap.ALWAYS}
-            />
-            <RecordKanban
-              emoji="🌙"
-              title="시작 전"
-              data={data?.applicationMap.NOT_STARTED}
-            />
-            <RecordKanban
-              emoji="🌞"
-              title="진행 중"
-              data={data?.applicationMap.IN_PROGRESS}
-            />
-            <RecordKanban
-              emoji="🌚"
-              title="완료됨"
-              data={data?.applicationMap.DONE}
-            />
-          </div>
-        ) : (
-          <div className="student-record-body">
-            {data?.applicationMap.applicationList.length > 0 ? (
-              <>
-                {data?.applicationMap.applicationList.map((a, index) => (
-                  <ReplyRecord
-                    emoji={a.emoji}
-                    title={a.title}
-                    repliedAt={a.repliedAt}
-                    status={a.status}
-                    key={index}
-                    replyId={a.replyId}
-                  />
-                ))}
-              </>
-            ) : (
-              <>
-                <p className="none">신청한 심청이 없습니다.</p>
-              </>
-            )}
-          </div>
-        )}
+        {isLoading ?
+          <Loading/>
+          :
+          data?.authority === "ROLE_TEACHER" ? (
+            <div className="record-body">
+              <RecordKanban
+                emoji="📌"
+                title="상시"
+                data={data?.applicationMap.ALWAYS}
+              />
+              <RecordKanban
+                emoji="🌙"
+                title="시작 전"
+                data={data?.applicationMap.NOT_STARTED}
+              />
+              <RecordKanban
+                emoji="🌞"
+                title="진행 중"
+                data={data?.applicationMap.IN_PROGRESS}
+              />
+              <RecordKanban
+                emoji="🌚"
+                title="완료됨"
+                data={data?.applicationMap.DONE}
+              />
+            </div>
+          ) : (
+            <div className="student-record-body">
+              {data?.applicationMap.applicationList.length > 0 ? (
+                <>
+                  {data?.applicationMap.applicationList.map((a, index) => (
+                    <ReplyRecord
+                      emoji={a.emoji}
+                      title={a.title}
+                      repliedAt={a.repliedAt}
+                      status={a.status}
+                      key={index}
+                      replyId={a.replyId}
+                    />
+                  ))}
+                </>
+              ) : (
+                <>
+                  <p className="none">신청한 심청이 없습니다.</p>
+                </>
+              )}
+            </div>
+          )
+        }
       </section>
     </>
   );
